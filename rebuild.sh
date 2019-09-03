721abdf6bffc50bd0d505281b0419a4b1f53e52f
Ian Allison <iana@pims.math.ca>
2019-09-03 13:54:31 -0700

    Change URL pattern
    Signed-off-by: Ian Allison <iana@pims.math.ca>

---
+++ rebuild.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -x
+# This is a total hack until we can get time/permission to rebuild the pages
+# within a single site.
+
+COLOR_END='\e[0m'
+COLOR_RED='\e[0;31m'
+
+ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SITES_DIR="${ROOT_DIR}/sites"
+
+SITES="sites/colloquia/2017 sites/colloquia/2018"
+
+msg_exit() {
+    printf "$COLOR_RED$@$COLOR_END"
+    printf "\n"
+    printf "Exiting...\n"
+    exit 1
+}
+
+cleanup() {
+    msg_exit "Site Update failed"
+}
+trap "cleanup" ERR INT TERM
+
+cd "${ROOT_DIR}"
+for site in ${SITES} ; do
+    source="${ROOT_DIR}/${site}"
+    if [ -d "${source}" ]; then
+
+        target="${ROOT_DIR}/static/$( basename "${site}" )"
+        
+        if [ -d "${target}" ] ; then
+            rm -rf "${target}"
+        fi
+        cd "${source}"
+        hugo --themesDir=${ROOT_DIR}/themes -d ${target}
+    else
+        echo "Unable to find source site ${source}"
+    fi
+done
+
+cd ${ROOT_DIR} 
+hugo
